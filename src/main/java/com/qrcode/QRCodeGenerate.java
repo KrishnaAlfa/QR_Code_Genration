5259cfbb1aebbb9e93a5db2dc693a4522fb13f75
unknown <thakur.krishna09@gmail.com>
2020-04-26 16:18:21 +0530

    read QR code

--- src/main/java/com/qrcode/QRCodeGenerate.java
+++ src/main/java/com/qrcode/QRCodeGenerate.java
@@ -1,19 +1,32 @@
 package com.qrcode;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 
+import javax.imageio.ImageIO;
+
 import org.springframework.stereotype.Component;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
 import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.QRCodeWriter;
 
 @Component
 public class QRCodeGenerate {
+	
+	// create QR code
 	public String getQRCode(QR_Code_information codeInformation) throws WriterException, IOException {
 		String QR_PATH = "C:\\Users\\Krishna\\Desktop\\QRCode\\";
 		String QRName = QR_PATH + codeInformation.getName() + "-QRCode.png";
@@ -25,5 +38,16 @@ public class QRCodeGenerate {
 		Path path = FileSystems.getDefault().getPath(QRName);
 		MatrixToImageWriter.writeToPath(matrix, "PNG", path);
 		return "QR code generated successfully.";
+	}
+	
+	
+	// Read QR code
+	public String readQRCode(String qrImage) throws Exception {
+		BufferedImage buffImage=ImageIO.read(new File(qrImage));
+		LuminanceSource lmSource= new BufferedImageLuminanceSource(buffImage);
+		BinaryBitmap bitMap=new BinaryBitmap(new HybridBinarizer(lmSource));
+		Result res = new MultiFormatReader().decode(bitMap);
+		return  res.getText();
+		
 	}
 }
--- src/main/java/com/qrcode/QrCodeExampleApplication.java
+++ src/main/java/com/qrcode/QrCodeExampleApplication.java
@@ -9,13 +9,18 @@ import com.google.zxing.WriterException;
 
 @SpringBootApplication
 public class QrCodeExampleApplication {
-	 
-	public static void main(String[] args) throws WriterException, IOException {
+	static String QR_PATH = "C:\\Users\\Krishna\\Desktop\\QRCode\\";
+	public static void main(String[] args) throws Exception {
 	
 		ApplicationContext applicationContext = SpringApplication.run(QrCodeExampleApplication.class, args);
 		
-		QRCodeGenerate service = applicationContext.getBean(QRCodeGenerate.class);
-		System.out.println(service.getQRCode((new QR_Code_information("Name = Krishna Pratap Singh","Address = Etawah uttar prdaesh","Degree = BTech","Age = 25","Mobile No. = 8957006128"))));
+		/*// write data from QR code
+		 * QRCodeGenerate service = applicationContext.getBean(QRCodeGenerate.class);
+		 System.out.println(service.getQRCode((new QR_Code_information("Name = Krishna Pratap Singh","Address = Etawah uttar prdaesh","Degree = BTech","Age = 25","Mobile No. = 8957006128"))));
+		*/
 		
+		// read data from QR code
+		QRCodeGenerate service = applicationContext.getBean(QRCodeGenerate.class);
+		System.out.println(service.readQRCode(QR_PATH+"Name = Krishna Pratap Singh-QRCode.png"));
 	}
 }
